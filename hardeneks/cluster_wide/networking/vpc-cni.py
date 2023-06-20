3301d6a6d2ad1d736dc36bfb7789b935c23ef3aa
jalawala <alawalajp@gmail.com>
2023-06-20 09:25:54 +0000

    update

--- hardeneks/cluster_wide/networking/vpc-cni.py
+++ hardeneks/cluster_wide/networking/vpc-cni.py
@@ -26,10 +26,33 @@ class deploy_vpc_cni_managed_add_on(Rule):
             #print(f"[bold][red]{exc}")
             Info = "VPC CNI is not Deployed as EKS Managed Add-On"
             Status = False
-
-        
+            
         self.result = Result(status=Status, resource_type="VPC CNI Managed Add-on",info=Info)    
 
         
 
+class use_separate_iam_role_for_cni(Rule):
+    _type = "cluster_wide"
+    pillar = "networking"
+    section = "vpc-cni"
+    message = "Use separate IAM role for CNI"
+    url = "https://aws.github.io/aws-eks-best-practices/scalability/docs/control-plane/#use-eks-124-or-above"
+
+
+    def check(self, resources: Resources):
+
+        daemonset = kubernetes.client.AppsV1Api().read_namespaced_daemon_set(name="aws-node", namespace="kube-system")
+        sa = daemonset.spec.template.spec.service_account_name
+        sa_data = kubernetes.client.CoreV1Api().read_namespaced_service_account(sa, 'kube-system', pretty="true")
+        #print(sa_data.metadata.annotations.keys())
         
+        if 'eks.amazonaws.com/role-arn' in sa_data.metadata.annotations.keys():
+            Status = True
+            Info = "VPC CNI uses separate IAM Role (IRSA)"
+        else:
+            Status = False
+            Info = "VPC CNI doesn't use separate IAM Role (IRSA)"            
+        
+        self.result = Result(status=Status, resource_type="IRSA for VPC CNI",info=Info)    
+    
+            
