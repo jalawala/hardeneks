dc47f7d8e6d0c89e0221a2ae90fe06db71f83c3f
jalawala <alawalajp@gmail.com>
2023-06-20 10:58:24 +0000

    update

--- hardeneks/cluster_wide/networking/vpc-cni.py
+++ hardeneks/cluster_wide/networking/vpc-cni.py
@@ -56,3 +56,40 @@ class use_separate_iam_role_for_cni(Rule):
         self.result = Result(status=Status, resource_type="IRSA for VPC CNI",info=Info)    
     
             
+class use_small_dedicated_cluster_subnets(Rule):
+    _type = "cluster_wide"
+    pillar = "networking"
+    section = "vpc-cni"
+    message = "Use dedicated subnets of size /28 for Cluster"
+    url = "https://aws.github.io/aws-eks-best-practices/scalability/docs/control-plane/#use-eks-124-or-above"
+
+
+    def check(self, resources: Resources):
+        Status = True
+    
+        eksclient = boto3.client("eks", region_name=resources.region)
+        cluster_metadata = eksclient.describe_cluster(name=resources.cluster)
+        vpcId  = cluster_metadata["cluster"]["resourcesVpcConfig"]["vpcId"]
+        subnetIds = cluster_metadata["cluster"]["resourcesVpcConfig"]["subnetIds"]
+    
+        #print("vpcId={} subnetIds={}".format(vpcId, subnetIds))
+        
+        #print("subnets={} subnet_ids={}".format(subnets, subnet_ids))
+        ec2client = boto3.client('ec2')
+        subnetsList = ec2client.describe_subnets(SubnetIds=subnetIds)
+        #print("response={} ".format(response))
+        
+        
+        Info = "Cluster Subnets : "
+        for subnet in subnetsList['Subnets']:
+            #print("SubnetId={} CidrBlock={} AvailableIpAddressCount={}".format(subnet['SubnetId'], subnet['CidrBlock'], subnet['AvailableIpAddressCount']))
+            
+            cidr_block = subnet['CidrBlock']
+            Info += " " + cidr_block
+            cidr_size = cidr_block.split('/')[-1]
+            #print(cidr_size)
+            if int(cidr_size) < 28:
+                Status = False
+                
+        self.result = Result(status=Status, resource_type="Cluster Subnets",info=Info)        
+        
