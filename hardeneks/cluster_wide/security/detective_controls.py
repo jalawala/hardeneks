fa74b9dc294ad3b6c4075195040adc9f342c24da
jalawala <alawalajp@gmail.com>
2023-06-23 10:49:19 +0000

    update

--- hardeneks/cluster_wide/security/detective_controls.py
+++ hardeneks/cluster_wide/security/detective_controls.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 from ...resources import Resources
 from hardeneks.rules import Rule, Result
@@ -16,6 +17,8 @@ class check_logs_are_enabled(Rule):
         Status = False
         Info = ""
         client = boto3.client("eks", region_name=resources.region)
+        guardduty_client = boto3.client('guardduty', region_name=resources.region       )
+        
         cluster_metadata = client.describe_cluster(name=resources.cluster)
         
         clusterLoggingTypes = cluster_metadata["cluster"]["logging"]["clusterLogging"]
@@ -30,6 +33,26 @@ class check_logs_are_enabled(Rule):
                 Info += " Logs Enabled : " + " ".join(logType['types'])
             else:   
                 Info += " Logs Disabled : " + " ".join(logType['types'])
+        
+        response = guardduty_client.list_detectors()
+        if 'DetectorIds' in response.keys():
+            detector_id = response['DetectorIds'][0]
+            
+        #print("detector_id={}".format(detector_id))
+        
+        # Check if EKS protection is enabled
+        response = guardduty_client.get_detector(DetectorId=detector_id)
+        
+        
+        #json_formatted_str = json.dumps(response, indent=4)
+        #print(json_formatted_str)
+        
+        guardDutyEKSAudiLogStatus = response['DataSources']['Kubernetes']['AuditLogs']['Status']
+        
+        if guardDutyEKSAudiLogStatus == 'ENABLED':
+            Status  = True
+        
+        Info += " guardDutyEKSAudiLogStatus = {}".format(guardDutyEKSAudiLogStatus)     
                 
         self.result = Result(status=Status, resource_type="Log Configuration", info=Info)
  
